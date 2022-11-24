114c2c5462ae7dc0ddb3ad99d29fffc8482b976d
JOKERadaw <valerioman05@gmail.com>
2022-11-24 14:49:03 +0100

    plsgodhelpme

---
+++ mazerun/Assets/Scripts/Maze.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    public int sizeX, sizeZ;
+
+    public Mazecell cellPrefab;
+
+    private Mazecell[,] cells;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
